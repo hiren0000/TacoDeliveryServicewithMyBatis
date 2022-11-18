412bbbcd5926d534eadd2c948d74d96c940539af
Hiren <Hiren@DESKTOP-Q4JDVS8>
2022-11-18 13:26:52 +0000

    Initial commit

---
+++ TacoDeliveryService-1/src/main/java/com/rebel/taco/TacoDeliveryService1Application.java
@@ -0,0 +1,13 @@
+package com.rebel.taco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TacoDeliveryService1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TacoDeliveryService1Application.class, args);
+	}
+
+}
---
+++ TacoDeliveryService-1/src/test/java/com/rebel/taco/TacoDeliveryService1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rebel.taco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TacoDeliveryService1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
